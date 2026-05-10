5827037e1713dc56121a90462d15dcd8c12ef6fc
buntyrdp <smartykhan484@gmail.com>
2026-05-10 18:08:08 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -17,8 +17,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'public',
-  OWNER_NAME: process.env.OWNER_NAME || "𓄂𐎓🐼мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3•┼⃖🐬",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "919883457657",
+  OWNER_NAME: process.env.OWNER_NAME || "𓄂𐎓🐼BunTyGodz┼⃖🐬",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923073022765",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
